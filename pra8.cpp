444a11680c7c15c073449a8a3e370fde69b92d02
qylyulong <52924357+qylyulong@users.noreply.github.com>
2020-05-29 09:13:43 +0800

    添加项目文件。

---
+++ pra8.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+//void swap(int*a, int*b)
+//{                      
+//    int temp;  
+//    temp =*a;
+//    *a = *b;
+//    *b = temp;
+//}
+int main()
+{
+    int i = 4, j = 8;
+    swap(i, j);
+    cout << i << "," << j << endl;
+    return 0;
+}    
+          
